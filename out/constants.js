f30d4f6f1b793a4705fe45dbf3a8b9d50a103cd7
Joe <greenivi@163.com>
2021-11-23 18:40:56 +0800

    refresh files

--- out/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NAMESPACE = void 0;
-exports.NAMESPACE = 'gitTreeCompare';
-//# sourceMappingURL=constants.js.map
--- out/git.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-//# sourceMappingURL=git.js.map
